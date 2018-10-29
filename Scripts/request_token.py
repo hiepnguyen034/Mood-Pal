8524b47a7739d2b910ba1c2c752619bbcc667680
hiepnguyen034 <hiepnguyen034@gmail.com>
2018-10-29 11:51:19 -0400

    clean up

--- Scripts/request_token.py
+++ Scripts/request_token.py
@@ -14,11 +14,8 @@ try:
 except ImportError:
     import urllib2
 import time
-<<<<<<< HEAD
 
 
-=======
->>>>>>> f82736c5b4ea6ea84230117914cfdd07c091dacc
 model = 'my_model.h5'
 graph_glob=Graph()
 with graph_glob.as_default():
